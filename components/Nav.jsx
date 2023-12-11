92c93242f28fe9a9a21b4acc34c267f3a477b89e
safakun <safarovdeem@gmail.com>
2023-12-11 09:05:58 +0500

    created nav component desktop navigation new

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -11,6 +11,8 @@ const Nav = () => {
   const isUserLoggedIn = true;
   const [providers, setProviders] = useState(null);
 
+  const [toggleDropdown, setToggleDropdown] = useState(false);
+
 
   useEffect(() => {
     const setProviders = async () => {
@@ -83,6 +85,65 @@ const Nav = () => {
       </div>
 
       {/* Mobile navigation */}
+      <div className="sm:hidden flex relative">
+        {isUserLoggedIn ? (
+          <div className="flex">
+ <Image src="/assets/images/logo.svg" 
+            width={37}
+            height={37}
+            className="rounded-full"
+            alt="profile"
+            onClick={() => {setToggleDropdown((prev) => !prev)}}
+            />
+
+            {toggleDropdown && (
+              <div className="dropdown">
+                <Link
+                href="/profile"
+                className="dropdown_link"
+                onClick={() => setToggleDropdown(false)}
+                >
+                My Profile
+                </Link>
+                <Link
+                href="/create_prompt"
+                className="dropdown_link"
+                onClick={() => setToggleDropdown(false)}
+                >
+                Create Prompt
+                </Link>
+
+                <button
+                type="button"
+                onClick={() => {
+                  setToggleDropdown(false);
+                  signOut();
+                }
+                }
+                className="mt-5 w-full black_btn"
+                >
+                  Sign Out
+                </button>
+              </div>
+            )}
+          </div>
+        ) : (
+          <>
+          {providers && 
+          Object.values(providers).map((provider) => (
+            <button
+            type="button"
+            key={provider.name}
+            onClick={() => signIn(provider.id)}
+            className="black_btn"
+            >
+              Sign In
+            </button>
+          ))
+          }
+          </>
+        )}
+      </div>
     </nav>
   )
 }
