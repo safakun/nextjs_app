ed244e37e7bfe1d9a5a0315319bf4640b25f75fd
safakun <safarovdeem@gmail.com>
2023-12-11 07:12:44 +0500

    initial commit

---
+++ app/layout.jsx
@@ -0,0 +1,23 @@
+import '@styles/globals.css';
+
+export const metadata = {
+    title: "Promtopia",
+    description: "Discover and share AI promts"
+}
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+        <body>
+        <div className="main">
+            <div className="gradient" />
+        </div>
+        <main className="app">
+            {children}
+        </main>
+        </body>
+    </html>
+  )
+}
+
+export default RootLayout
